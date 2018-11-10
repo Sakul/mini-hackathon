3181be87d95a9f5b5e41e2184722b39ec0e0329b
aphaphon <pui.aphaphon@gmail.com>
2018-11-10 12:03:06 +0700

    Compute normal case only baht.

--- Round01/netcore/FirstRound.Lib/moa.cs
+++ Round01/netcore/FirstRound.Lib/moa.cs
@@ -8,7 +8,15 @@ namespace FirstRound.Lib
     {
         public int ComputeChange(double totalAmount, double customerPayment)
         {
-            throw new NotImplementedException();
+            double result = totalAmount - customerPayment;
+            double afterDot = result - (long)result;
+
+            if (totalAmount <= customerPayment)
+            {
+                return Convert.ToInt32(Math.Pow(result, 2.0));
+            }
+
+            return Convert.ToInt32(totalAmount);
         }
 
         public ChangeSolution GetChangeBankNotesAndCoins(int changeInSatang)
