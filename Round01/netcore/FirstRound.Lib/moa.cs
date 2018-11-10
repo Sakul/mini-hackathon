3584d077431559054f869b7d1185191a02f69180
mnswstt <manutsawee113@gmail.com>
2018-11-10 12:20:34 +0700

    Currectly comding.

--- Round01/netcore/FirstRound.Lib/moa.cs
+++ Round01/netcore/FirstRound.Lib/moa.cs
@@ -10,6 +10,26 @@ namespace FirstRound.Lib
         {
             double result = totalAmount - customerPayment;
             double afterDot = result - (long)result;
+            //0.34 < 0.01  25 -> 25
+            //0.34 < 0.25, 25 -> 50
+            //0.34 < 0.50, 51 -> 75
+            //0.51 < 0.75, 76 -> 1;
+            if (afterDot < 1e-2)
+            {
+
+            }
+            else if (afterDot < .25)
+            {
+
+            }
+            else if (afterDot < .50)
+            {
+
+            }
+            else if (afterDot < .75)
+            {
+
+            }
 
             if (totalAmount <= customerPayment)
             {
