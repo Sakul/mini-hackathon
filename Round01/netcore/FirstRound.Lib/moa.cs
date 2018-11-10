d8deed6caf250f477cec959c0873e80bbd23340f
aphaphon <pui.aphaphon@gmail.com>
2018-11-10 11:59:29 +0700

    Initial implements.

---
+++ Round01/netcore/FirstRound.Lib/moa.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FirstRound.Lib
+{
+    class moa : IVerySimplePOS
+    {
+        public int ComputeChange(double totalAmount, double customerPayment)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ChangeSolution GetChangeBankNotesAndCoins(int changeInSatang)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
